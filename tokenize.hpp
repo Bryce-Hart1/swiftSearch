b73894355af3e42686f7144b26e15fb92dcbabfd
Bryce Hart <bryce.hart23@gmail.com>
2026-01-16 23:03:30 -0500

    deleting fileTree for now

--- tokenize.hpp
+++ tokenize.hpp
@@ -6,49 +6,14 @@
 #include <memory>
 #include<vector>
 
-class fileTreeNode{
-    private:
-    std::string path;
-    std::vector<std::unique_ptr<fileTreeNode>> children;
-    bool isChild;
-    public:
-    std::string getPath();
-    fileTreeNode();
-    fileTreeNode(const std::string& name);
-};
-
-
-//this will create a class based of the fileTreeNode given. 
-
-class fileTree{
-    friend class fileTreeNode; 
-
-
-    private:
-        fileTreeNode root;
-        size_t size;
-
-    public:
-        fileTree();
-        fileTree(std::string root);
-        fileTreeNode getDirectoryRoot();
-        std::queue<std::string> queueToVisit(std::string root);
-        bool isChildNode(std::string path);
-
-
-
-
-
-
-};
 
 std::string toLowerCase(std::string str);
 
-std::string makeSpellCheck(std::string input, std::vector<std::string> expected);
+std::string makeSpellCheck(std::string input, std::vector<std::string> &expected);
 
 int checkTokenValid(int place, std::string tokenExtracted);
 
 
-void tokenMain(std::string arg);
+void tokenMain(std::string input);
 
 
