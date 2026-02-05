0d76b09316815b5301675384e144e5af2de2d558
Bryce Hart <bryce.hart23@gmail.com>
2026-02-05 13:24:44 -0500

    Added more comments for readers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include"parse.hpp"
-#include"tokenize.hpp"
-#include"classes.hpp"
-#include"constants.hpp"
+#include"parse.hpp" //check this cpp for all operatons done by the threads, and how they are assigned.
+#include"tokenize.hpp" //check this cpp for how the tokenizer parses user input
+#include"classes.hpp" //check this cpp for all class implementations 
+#include"constants.hpp" //check this for print:: namespace, boolean flags and other global functions
 
 
 //just checks length, not validity. If not valid, return after telling the user it is wrong
@@ -9,7 +9,6 @@ bool goodArg(const std::string &argumentArr){
     if(argumentArr.empty()){
         return false;
     }
-    
     size_t wordCount = 0;
     bool inWord = false;
     
@@ -19,14 +18,14 @@ bool goodArg(const std::string &argumentArr){
                 wordCount++;
                 inWord = true;
             }
-        } else {
+         }else{
             inWord = false;
         }
     }
-    
     return wordCount >= 2;
 }
 
+//checks length/gets string from user. Enters a loop till user enters one of correct length
 std::string boot(){
     std::string arguments;
     print::Logo();
@@ -45,13 +44,14 @@ std::string boot(){
     }
     return arguments;
 }
+
 int main(){
     std::string argumentArr = boot();
     Timer mainWatch;
     mainWatch.start(); //start timer for how long program takes
     fileTreeStructure fileStruct = *tokenize(argumentArr, std::ref(mainWatch)); //all tokens get assigned here 
     assignOperation(operationTypeOfParse, fileStruct.createFileQueue()); //all work gets done here
-    mainWatch.stop(); //stop and print timer
+    mainWatch.stop(); //stop and print
     print::User("Program has completed in " + mainWatch.readableTime());
     print::Debug("Program sucessfully exited");
 }
